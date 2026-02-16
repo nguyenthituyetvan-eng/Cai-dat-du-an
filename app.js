6e4e2668e1b4dfbb3fc96ccc526b273f69ff15b7
Nguyen Thi Tuyet Van <vanb2308404@student.ctu.edu.vn>
2026-02-16 11:00:29 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const contactsRouter = require("./app/routers/contact.route");
+const ApiError = require("./app/api-error");
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application. " });
 });
 app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+app.use((error, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 module.exports = app;
